8b3e689d458d195dababb38dc3ece6467b96295e
natoliveira <nataliadeolive@gmail.com>
2021-06-28 10:35:56 -0300

    implementacao coletaMoedas

---
+++ cmake-build-debug/CMakeFiles/untitled32.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled32.dir/coletaMoedas.cpp.o"
+  "untitled32"
+  "untitled32.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
